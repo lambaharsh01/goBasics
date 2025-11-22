d6e32a84fa2b0d209c71822a294fbb8dc504a945
Harsh <118607392+lambaharsh01@users.noreply.github.com>
2025-11-22 19:02:10 +0530

    Update variable_declaration.go

--- Interviews/variable_declaration.go
+++ Interviews/variable_declaration.go
@@ -5,8 +5,7 @@ import (
 )
 
 func VariableDeclaration() {
-
-
+	
 	var x, y, z = 1, 2.4, "welcome"
 	// x, y, z := 1, 2.4, "welcome"
 
@@ -14,11 +13,39 @@ func VariableDeclaration() {
 	fmt.Println(y)
 	fmt.Println(z)
 
-
 	strArr := [2]string{} // initializes and empty ["", ""] string array implicitly
 	strArr[0]="Harsh"
 	strArr[1]="Lamba"
 
 	fmt.Println(strArr)
+
+
+	
+
+	// EMPTY ANY VARIABLE DECLERATION
+	
+	var anyVar interface{} // OR any
+	
+	if anyVar == nil {
+		println("well it's nil alright")
+	} else {
+		println("naa not nil")
+	}
+
+	// 1. Can only perform assignment = 
+	// 2. Comparison == operations can be done 
+	// 3. No arithmetic operations 
+	// 4. Can't access fields or methods a.Name  or a[0]
+	// 5. Can't pass it to a function expecting a specific type // fmt.Println(len(a))
+
+	// 6. TYPE SWITCH CAN BE DONE 
+	switch v := a.(type) {
+	case int:
+		fmt.Println("int:", v)
+	case string:
+		fmt.Println("string:", v)
+	default:
+		fmt.Println("unknown type")
+	}
 
 }
